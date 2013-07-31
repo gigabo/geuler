75bcac5daf07a71124a3ade70624ccf6ea0bc6fd
Bo Borgerson <gigabo@gmail.com>
2013-07-31 11:01:27 -0700

    Solve problem 1

--- geuler.go
+++ geuler.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+var solutions = map[string]func() string{
+	"1": func() string {
+		n := 0
+		for i := 0; i < 1000; i++ {
+			if (i%3) == 0 || (i%5) == 0 {
+				n += i
+			}
+		}
+		return fmt.Sprintf("%d", n)
+	},
+}
+
+func run(n string) string {
+	return fmt.Sprintf("%v:\t%s", n, solutions[n]())
+}
 
 func main() {
-	fmt.Printf("Hello, World! %d\n", fib(4))
+	fmt.Println(run(os.Args[1]))
 }
