a909a51085f41deb9ad0cb447234d12c497091ef
Bo Borgerson <gigabo@gmail.com>
2013-07-31 21:48:25 -0700

    Simplify the naive largestPrimeFactor implementation

--- factor.go
+++ factor.go
@@ -11,10 +11,7 @@ func isPrime(n uint) bool {
 
 func largestPrimeFactor(n uint) uint {
 	var fact uint
-	if isPrime(n) {
-		return n
-	}
-	for i := uint(2); i <= n/2+1; i++ {
+	for i := uint(2); i <= n; i++ {
 		if n%i == 0 && isPrime(i) {
 			fact = i
 		}
