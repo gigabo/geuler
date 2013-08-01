e4c41ab2fff4b4f1ce38977e63eecab283e301d3
Bo Borgerson <gigabo@gmail.com>
2013-07-31 20:22:08 -0700

    Faster fibonacci

--- fib.go
+++ fib.go
@@ -1,8 +1,20 @@
 package main
 
+var memo []uint
+
 func fib(n uint) uint {
-	if n < 2 {
-		return 1
+	if len(memo) <= int(n) {
+		newMemo := make([]uint, n+1)
+		copy(newMemo, memo)
+		memo = newMemo
 	}
-	return fib(n-1) + fib(n-2)
+	res := memo[n]
+	if res != 0 {
+	} else if n < 2 {
+		res = 1
+	} else {
+		res = fib(n-1) + fib(n-2)
+	}
+	memo[n] = res
+	return res
 }
